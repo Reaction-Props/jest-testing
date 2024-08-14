4a1bca0104bd609ca7c5819dffdee182433d1342
React-Props <daniel.linker@intellicode-solutions.de>
2024-08-14 22:59:53 +0200

    describe

--- multiply/multiply.test.js
+++ multiply/multiply.test.js
@@ -1,8 +1,25 @@
 import {multiply} from "./multiply";
 
 
-test('multiply', () => {
-    const res = multiply(2, 3)
+describe('multiply', () => {
+    it('sollte eine multiplizierte positive Zahl sein', () => {
+        //AAA - arrange, act, assert
+        const expectedResult = 6;
 
-    expect(res).toBe(6)
+        const actualResult = multiply(3, 2);
+
+        expect(actualResult).toBe(expectedResult)
+    });
+
+    it('sollte eine multiplizierte negative Zahl sein', () => {
+        const res = multiply(-3, -2);
+        expect(res).toBe(6)
+    });
+
+    it('sollte eine multiplizierte negative und positive Zahl sein', () => {
+        const res = multiply(3, -2);
+        expect(res).toBe(-6)
+    });
+
+
 })
